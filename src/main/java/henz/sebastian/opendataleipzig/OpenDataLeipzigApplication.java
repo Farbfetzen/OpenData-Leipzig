78dfcf7380a4eb779f5fb1db0aefbee0c5da57fa
bastihz <bastihz.dev@posteo.de>
2021-06-26 18:35:36 +0200

    Initial commit

---
+++ src/main/java/henz/sebastian/opendataleipzig/OpenDataLeipzigApplication.java
@@ -0,0 +1,14 @@
+package henz.sebastian.opendataleipzig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenDataLeipzigApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Hello World");
+		//SpringApplication.run(OpenDataLeipzigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/henz/sebastian/opendataleipzig/OpenDataLeipzigApplicationTests.java
@@ -0,0 +1,13 @@
+package henz.sebastian.opendataleipzig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenDataLeipzigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
