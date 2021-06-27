7e4cfe66290437b7401dcd6d5071e2dff9d03156
bastihz <bastihz.dev@posteo.de>
2021-06-27 14:56:06 +0200

    Populate database from xml file via POST update

--- src/main/java/henz/sebastian/opendataleipzig/StreetController.java
+++ src/main/java/henz/sebastian/opendataleipzig/StreetController.java
@@ -1,10 +1,16 @@
 package henz.sebastian.opendataleipzig;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -16,7 +22,30 @@ public class StreetController {
 
     @PostMapping(path = "/update")
     @ResponseStatus(HttpStatus.OK)
-    public void update() {}
+    public void update() {
+
+        // Populate the database from an xml file. This is done
+        // for development purposes. Later it will download the data
+        // from the web.
+        // The data is parsed sequentially. This is inefficient and
+        // should be done with a batch method.
+        // TODO: Learn how to do batch xml parsing.
+
+        final File file = new File("data/Strassenverzeichnis.xml");
+        try {
+            final XmlMapper xmlMapper = new XmlMapper();
+            final JsonNode streetArrayNode = xmlMapper.readTree(file).get("STRASSE");
+            for (final JsonNode streetNode : streetArrayNode) {
+                final Street street = new Street();
+                final JsonNode coreDataNode = streetNode.get("STAMMDATEN");
+                street.setName(coreDataNode.get("NAME").asText());
+                street.setKey(coreDataNode.get("SCHLUESSEL").asText());
+                streetRepository.save(street);
+            }
+        } catch(final IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @GetMapping(path = "/getall")
     @ResponseBody
