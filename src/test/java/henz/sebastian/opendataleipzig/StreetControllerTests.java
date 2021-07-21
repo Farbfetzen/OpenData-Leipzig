4d37b293e5d62ef65276afcc88606ae87c8e40d9
bastihz <bastihz.dev@posteo.de>
2021-07-21 14:08:11 +0200

    Define fixed port for tests

--- src/test/java/henz/sebastian/opendataleipzig/StreetControllerTests.java
+++ src/test/java/henz/sebastian/opendataleipzig/StreetControllerTests.java
@@ -3,29 +3,26 @@ package henz.sebastian.opendataleipzig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class StreetControllerTests {
-
-    @LocalServerPort
-    private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
 
-    private static final String path = "http://localhost:%d/streets%s";
+    private final String path;
+
+    StreetControllerTests(@Value("${server.port}") final int port) {
+        path = "http://localhost:" + port + "/streets";
+    }
 
     @Test
     void updateReturnsCorrectStatusAndBody() {
-        final ResponseEntity<String> response = restTemplate.postForEntity(
-            String.format(path, port, "/update"),
-            null,
-            String.class
-        );
+        final ResponseEntity<String> response = restTemplate.postForEntity(path + "/update", null, String.class);
         Assertions.assertAll(
             () -> Assertions.assertEquals(201, response.getStatusCodeValue()),
             () -> Assertions.assertEquals("Updated database with 3054 records.", response.getBody())
